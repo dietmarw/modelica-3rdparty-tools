f503979d8558de5c780e21a823d26b4aabc0c605
Dietmar Winkler <dietmar.winkler@dwe.no>
2014-04-16 10:38:33 +0200

    Improves the output messages.

--- updateMirrors.sh
+++ updateMirrors.sh
@@ -14,10 +14,10 @@ cd $libDir
 
 for LIB in `ls -F |grep / | sed 's#/$##'`; do
     cd "$libDir/$LIB"
+    echo "Syncing updates of $LIB :"
     git remote | grep 'modelica-3rdparty' && \
-    	(echo "Syncing updates of $LIB ..."; \
-	git remote update; \
-	git push --mirror modelica-3rdparty -q) || \
-	(echo "Looks like $LIB is not a fork, so only pulling updates."; \
-	git remote update --prune;)
+       (git remote update; \
+        git push --mirror modelica-3rdparty -q) || \
+        (echo "Looks like $LIB is not a fork, so only pulling updates."; \
+        git remote update --prune;)
 done;
